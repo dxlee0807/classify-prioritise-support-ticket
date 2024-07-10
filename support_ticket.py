0249ddd1654d7484fdab2dda73c8bf98dc4b7d4a
dxlee0807 <80621315+dxlee0807@users.noreply.github.com>
2024-07-10 02:01:52 +0000

    upload ml model and topic csv

---
+++ support_ticket.py
@@ -0,0 +1,21 @@
+# expected file: 
+# - tuned_lr.pkl, 
+# - [topic_lda, topic_lda.id2word] to get topic information (actually need turn it into df and load)
+
+# Financial Domain Complaint Ticketing System
+
+# try using generative AI to generate some (3-5) financial complaint tickets
+
+# enter title [short_text]
+# enter description [long_text]
+
+# preprocess the description
+# - nlp, clean, tfidf
+# - model.predict() to get ticket category, and priority
+
+
+# can select which support user/team
+# table (sortable and filterable)
+# Description | Category | Priority | Resolve (tick to clear)
+
+
