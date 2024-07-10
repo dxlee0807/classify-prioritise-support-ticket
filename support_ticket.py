4fd18ec3fcf17b73b7cd0bf218c2bb8a75859fb8
dxlee0807 <80621315+dxlee0807@users.noreply.github.com>
2024-07-10 04:12:11 +0000

    try setting python version

--- support_ticket.py
+++ support_ticket.py
@@ -2,6 +2,22 @@
 # - tuned_lr.pkl, 
 # - [topic_lda, topic_lda.id2word] to get topic information (actually need turn it into df and load)
 
+# import packages
+import os
+from pickle import load
+import pandas as pd
+import sklearn
+from sklearn.linear_model import LogisticRegression
+
+# load topic_csv
+topic_df = pd.read_csv(os.path.join(os.getcwd(),'data',"lda_topic.csv"))
+print(topic_df)
+
+
+# load tuned_lr.pkl
+with open(os.path.join(os.getcwd(),'data',"tuned_lr.pkl"), "rb") as f:
+    clf = load(f)
+
 # Financial Domain Complaint Ticketing System
 
 # try using generative AI to generate some (3-5) financial complaint tickets
